35b69c71e87870b95cf7c23fd8bf7e9afff690f3
NaJongHan <skwhdgks@gmail.com>
2025-12-15 23:02:39 +0900

    refactor: remove WriteNewsSheet component from cherished index for code cleanup
    - Deleted the WriteNewsSheet component to streamline the cherished module.
    - Updated index.ts to reflect the removal, ensuring only relevant components are exported.

--- components/cherished/index.ts
+++ components/cherished/index.ts
@@ -1,4 +1,3 @@
-export { default as WriteNewsSheet } from './WriteNewsSheet';
 export { default as PriorityReminderBanner } from './PriorityReminderBanner';
 export { default as CherishedPeopleList } from './CherishedPeopleList';
 export { default as AddPersonModal } from './AddPersonModal';
